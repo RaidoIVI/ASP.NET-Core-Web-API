7ea86ae852acbfddf85449d09437e336931cc664
Ivan <kiba82@gmail.com>
2022-11-04 04:43:43 +0300

    Пока пауза, утром контроллеры настрою и допишу

---
+++ Storage/Storage/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Storage
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<ISavedData>? SavedDatas { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connect = "workstation id=Storage.mssql.somee.com;packet size=4096;user id=Storage;pwd=tRg81zFpn;data source=Storage.mssql.somee.com;persist security info=False;initial catalog=Storage";
+                optionsBuilder.UseSqlServer(connect);
+                return;
+            }
+        }
+    }
+}
---
+++ Storage/Storage/ISavedData.cs
@@ -0,0 +1,24 @@
+﻿namespace Storage
+{
+    public interface ISavedData
+    {
+        int ID { get; set; }
+        string CompanyName { get; set; }
+        DateOnly Date { get; set; }
+        decimal НематериальныеСредства { get; set; }
+        decimal ОсновныеСредства { get; set; }
+        decimal КапитальныеВложения { get; set; }
+        decimal ЗапасыТМЦ { get; set; }
+        decimal ДебиторскаяЗадолженность { get; set; }
+        decimal ФинансовыеВложения { get; set; }
+        decimal ПрочиеВложения { get; set; }
+        decimal УставнойКапитал { get; set; }
+        decimal ФондыИРезервы { get; set; }
+        decimal Прибыль { get; set; }
+        decimal КредитыИЗаймы { get; set; }
+        decimal КредиторскаяЗадолженность { get; set; }
+        decimal РасчетыСПерсоналом { get; set; }
+        decimal ЗадолженностьПоНалогам { get; set; }
+        decimal ПрочиеОбязательства { get; set; }
+    }
+}
